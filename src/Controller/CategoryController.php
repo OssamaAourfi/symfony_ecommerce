d575814c919ff1a1d0c599bd15f023310a52c948
Aourfi Ossama <aourfidev@gmail.com>
2024-11-01 10:56:33 +0100

    addRoles

--- src/Controller/CategoryController.php
+++ src/Controller/CategoryController.php
@@ -13,7 +13,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class CategoryController extends AbstractController
 {
-    #[Route('/category', name:'category_index')]
+    #[Route('/admin/category', name:'category_index')]
     public function index(CategoryRepository $categoryRepository): Response
     {
         $categories = $categoryRepository->findAll();
@@ -22,7 +22,7 @@ class CategoryController extends AbstractController
         ]);
     }
     
-    #[Route('/category/new',name:'category_create')]
+    #[Route('/admin/category/new',name:'category_create')]
     public function create(EntityManagerInterface $em,Request $request):Response{
         $category = new Category();
         
@@ -31,27 +31,30 @@ class CategoryController extends AbstractController
         if($form->isSubmitted() && $form->isValid()){         
             $em->persist($category);
             $em->flush();
+            $this->addFlash('success','votre categorie est créer');
             return $this->redirectToRoute('category_index');
         }
         return $this->render('category/new.html.twig',['form' => $form->createView()]);
         
     }
     
-    #[Route('/category/{id}/update',name:'category_update')]
+    #[Route('/admin/category/{id}/update',name:'category_update')]
     public function edit(Category $category,EntityManagerInterface $em,Request $request ):Response{
         $form = $this->createForm(CategoryFormType::class, $category);
         $form->handleRequest($request);
         if($form->isSubmitted()&&$form->isValid()){
             $em->flush();
+            $this->addFlash('info', 'votre categorie est modifier');
           return  $this->redirectToRoute('category_index');
         }
         return $this->render('category/edit.html.twig', ['form' => $form->createView()]);
     }
     
-    #[Route('/category/{id}/delete',name:'category_delete')]
+    #[Route('/admin/category/{id}/delete',name:'category_delete')]
     public function delete (EntityManagerInterface $em,Category $category):Response{
         $em->remove($category);
         $em->flush();
+        $this->addFlash('danger', 'votre categorie est supprimer');
         return $this->redirectToRoute('category_index');
     }
 }
