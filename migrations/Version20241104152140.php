7c6d7c9a937d5a8e12a22fff69568e2c196ce129
Aourfi Ossama <aourfidev@gmail.com>
2024-11-14 14:37:42 +0100

    gestionstock

---
+++ migrations/Version20241104152140.php
@@ -0,0 +1,37 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20241104152140 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE product CHANGE image image VARCHAR(255) DEFAULT NULL');
+        $this->addSql('CREATE UNIQUE INDEX UNIQ_D34A04AD5E237E06 ON product (name)');
+        $this->addSql('ALTER TABLE user CHANGE roles roles JSON NOT NULL');
+        $this->addSql('ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT NULL COMMENT \'(DC2Type:datetime_immutable)\'');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE messenger_messages CHANGE delivered_at delivered_at DATETIME DEFAULT \'NULL\' COMMENT \'(DC2Type:datetime_immutable)\'');
+        $this->addSql('DROP INDEX UNIQ_D34A04AD5E237E06 ON product');
+        $this->addSql('ALTER TABLE product CHANGE image image VARCHAR(255) DEFAULT \'NULL\'');
+        $this->addSql('ALTER TABLE user CHANGE roles roles LONGTEXT NOT NULL COLLATE `utf8mb4_bin`');
+    }
+}
--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -2,8 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\AddProductHistory;
 use App\Entity\Product;
+use App\Form\AddProductHistoryType;
 use App\Form\ProductType;
+use App\Repository\AddProductHistoryRepository;
 use App\Repository\ProductRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -50,6 +53,14 @@ final class ProductController extends AbstractController
             $entityManager->persist($product);
             $entityManager->flush();
 
+            $stockHistory=new AddProductHistory();
+            $stockHistory->setQte($product->getStock());
+            $stockHistory->setProduct($product);
+            $stockHistory->setCreatedAt(new \DateTimeImmutable());
+
+            $entityManager->persist($stockHistory);
+            $entityManager->flush();
+            
             return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
         }
 
@@ -94,5 +105,36 @@ final class ProductController extends AbstractController
         }
 
         return $this->redirectToRoute('app_product_index', [], Response::HTTP_SEE_OTHER);
+    }
+    #[Route('/add/product/{id}/stock',name:'app_product_stock_add',methods:['GET','POST'])]
+    public function addStock($id,EntityManagerInterface $em,Request $request,ProductRepository $productRepository):Response{
+        $addStock = new AddProductHistory();
+        $form=$this->createForm(AddProductHistoryType::class,$addStock);
+        $form->handleRequest($request);
+        $product = $productRepository->find($id);
+        if($form->isSubmitted() && $form->isValid()){
+            if($addStock->getQte()>0){
+                $newQte= $product->getStock()+ $addStock->getQte();
+                $product->setStock($newQte);
+                $addStock->setCreatedAt(new \DateTimeImmutable());
+                $addStock->setProduct($product);
+                $em->persist($addStock);
+                $em->flush();
+
+                return $this->redirectToRoute("app_product_index");
+            }
+        }
+        
+        return $this->render('product/addStock.html.twig',
+            ['form'=>$form->createView(),
+              'product'=>$product  
+            ]
+    );
+    }
+    #[Route('/add/product/{id}/stock/history',name:'app_product_stock_add_history',methods:['GET'])]
+    public function productAddHistory($id,ProductRepository $productRepository,AddProductHistoryRepository $addProductHistoryRepository): Response{
+        $product = $productRepository->find($id);
+        $productAddedHistory = $addProductHistoryRepository->findBy(['product'=>$product],['id'=>'DESC']); 
+        return $this->render('product/added_stock_history.html.twig',['productsAdded'=>$productAddedHistory]);       
     }
 }
